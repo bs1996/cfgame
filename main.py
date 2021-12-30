eede0b1b3dc125c796950041163863df933c388d
bartek <bartekstefaniak@hotmail.com>
2021-12-30 18:18:22 +0100

    main window

--- main.py
+++ main.py
@@ -2,15 +2,24 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import socket
+import pygame
+import client
+menuwindow = True
+
+menu = pygame.display.set_mode((300,600))
+pygame.display.set_caption("cfgame-MENU")
+while menuwindow:
+    for event1 in pygame.event.get():
+        if event1.type == pygame.KEYDOWN:
+            if event1.key == pygame.K_KP_ENTER:
+                client.client()
+        # only do something if the event is of type QUIT
+        if event1.type == pygame.QUIT:
+            # change the value to False, to exit the main loop
+            menuwindow = False
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
