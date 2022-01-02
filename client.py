10a6ac82a09d670dd72292347313a8caa544c05d
bartek <bartekstefaniak@hotmail.com>
2022-01-02 01:11:48 +0100

    -

--- client.py
+++ client.py
@@ -1,16 +1,25 @@
 import pygame,sys,numpy
 import socket
 def client():
-
     pygame.init()
+    pygame.font.init()
+    pygame.font.get_init()
+    font2 = pygame.font.SysFont('chalkduster.ttf', 40)
+    text1 = font2.render('player 2 win !!', True, (0, 255, 0))
+    text2 = font2.render('player 1 win !!', True, (0, 255, 0))
+    textRect1 = text1.get_rect()
+    textRect1.center = (300, 300)
+    textRect2 = text2.get_rect()
+    textRect2.center = (300, 300)
+
     screen = pygame.display.set_mode((600,600))
     # define a variable to control the main loop
     running = True
     pygame.display.set_caption("cfgame")
     x1 = 50
     y1 = 50
-    x2=400
-    y2=400
+    x2 = 400
+    y2 = 400
     xp1 = 0.5
     yp1 = 0.0
     xp2 = 0.5
@@ -49,37 +58,42 @@ def client():
             y2 = y2 + yp2
             point1 = numpy.array([x1, y1])
             point2 = numpy.array([x2, y2])
-            road1.append(point1)
-            road2.append(point2)
-            for i in road1[0:-2]:
+            for i in road1[0:-1]:
                 if numpy.array_equal(i,point1):
                     gameover=1
                     print(i)
                     print(point1)
                     print('gameover')
-            for j in road2[0:-2]:
+                    screen.blit(text1, textRect1)
+
+            for j in road2:
                 if numpy.array_equal(j,point1):
                     gameover=1
                     print('gameover')
-            for i in road2[0:-2]:
+                    screen.blit(text1, textRect1)
+
+            for i in road2[0:-1]:
                 if numpy.array_equal(i,point2):
                     gameover=1
                     print(i)
                     print(point1)
                     print('gameover')
-            for j in road1[0:-2]:
+                    screen.blit(text2, textRect2)
+
+            for j in road1:
                 if numpy.array_equal(j,point2):
                     gameover=1
                     print('gameover')
+                    screen.blit(text2, textRect2)
+
+            road1.append(point1)
+            road2.append(point2)
         pygame.display.update()
         clock=pygame.time.Clock()
         clock.tick(100)
         t=t+0.01
         for event in pygame.event.get():
-
             if event.type == pygame.KEYDOWN:
-
-
                 if event.key == pygame.K_DOWN and direction1 == 0:
                     xp1 = 0.0
                     yp1 = 0.5
