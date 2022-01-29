53572f10bf5dcc6b6439dab65fbf1c6e82fad5d2
bartek <bartekstefaniak@hotmail.com>
2022-01-29 01:38:10 +0100

    -

--- server.py
+++ server.py
@@ -1,4 +1,19 @@
-import pygame,socket
-def server():
-    pygame.init()
-    screen = pygame.display.set_mode((600,600))
+import pygame,socket,numpy
+def server(player):
+   s=socket.socket()
+   port = 4000
+   s.bind('',port)
+   s.listen(5)
+   while True:
+      # Establish connection with client.
+      c, addr = s.accept()
+      print('Got connection from', addr)
+
+      # send a thank you message to the client. encoding to send byte type.
+      c.send('Thank you for connecting'.encode())
+
+      # Close the connection with the client
+      c.close()
+
+      # Breaking once connection closed
+      break
