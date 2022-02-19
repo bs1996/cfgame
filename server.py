1614fa2dfc10d08d11e487a0df0d99f5b38d09fe
bartek <bartekstefaniak@hotmail.com>
2022-02-19 17:03:05 +0100

    connection test

--- server.py
+++ server.py
@@ -1,9 +1,27 @@
 import pygame,socket,numpy
 def server(player):
-   s=socket.socket()
-   port = 4000
-   s.bind('',port)
+   # next create a socket object
+   s = socket.socket()
+   print("Socket successfully created")
+
+   # reserve a port on your computer in our
+   # case it is 12345 but it can be anything
+   port = 12345
+
+   # Next bind to the port
+   # we have not typed any ip in the ip field
+   # instead we have inputted an empty string
+   # this makes the server listen to requests
+   # coming from other computers on the network
+   s.bind(('', port))
+   print("socket binded to %s" % (port))
+
+   # put the socket into listening mode
    s.listen(5)
+   print("socket is listening")
+
+   # a forever loop until we interrupt it or
+   # an error occurs
    while True:
       # Establish connection with client.
       c, addr = s.accept()
