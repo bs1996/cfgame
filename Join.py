ba26711fc450959ea903bfaa51ed506df5dc5e54
bartek <bartekstefaniak@hotmail.com>
2022-03-04 20:55:59 +0100

    data exchange test - Receiving number of players

--- Join.py
+++ Join.py
@@ -1,17 +1,17 @@
-import pygame,socket,numpy
+import pygame,socket,numpy,Players
 def join(ip):
     import socket
+
     message=input('write message')
     # Create a socket object
     s = socket.socket()
-
     # Define the port on which you want to connect
     port = 12345
-
     # connect to the server on local computer
     s.connect((ip, port))
 
     # receive data from the server and decoding to get the string.
+    print(s.recv(1024).decode())
     print(s.recv(1024).decode())
     s.send(message.encode())
     # close the connection
