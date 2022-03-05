30d3e86f124550829b658321840c8372fd2ffb4e
bartek <bartekstefaniak@hotmail.com>
2022-03-05 15:35:47 +0100

    multithreading - test

--- Join.py
+++ Join.py
@@ -3,22 +3,19 @@ from threading import Thread
 def join(ip):
     import socket
     exchange = 1
-    while exchange:
-        message=input('write message')
-        # Create a socket object
-        s = socket.socket()
-        # Define the port on which you want to connect
-        port = 12345
-        # connect to the server on local computer
-        s.connect((ip, port))
+    message=input('write message')
+    # Create a socket object
+    s = socket.socket()
+    # Define the port on which you want to connect
+    port = 12345
+    # connect to the server on local computer
+    s.connect((ip, port))
 
-        # receive data from the server and decoding to get the string.
-        print(s.recv(1024).decode())
-        print(s.recv(1024).decode())
-        s.send(message.encode())
-        # close the connection
-        print('0-close 1-continue')
-        input(exchange)
-        if exchange == 0 :
-            s.send('close'.encode())
+    # receive data from the server and decoding to get the string.
+    print(s.recv(1024).decode())
+    print(s.recv(1024).decode())
+    s.send(message.encode())
+    # close the connection
+    print('0-close 1-continue')
+    s.send('close'.encode())
     s.close()
