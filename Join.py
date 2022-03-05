d11e623e013517fd6b796556d65b36a212ca0331
bartek <bartekstefaniak@hotmail.com>
2022-03-05 23:54:10 +0100

    multithreading test

--- Join.py
+++ Join.py
@@ -2,42 +2,35 @@ import pygame,socket,numpy,Players ,threading
 from threading import Thread
 def join(ip):
 
+    # Define the port on which you want to connect
+    port = 12345
 
-    def Main():
-        # local host IP '127.0.0.1'
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+    # connect to server on local computer
+    s.connect((ip, port))
 
-        # Define the port on which you want to connect
-        port = 12345
+    # message you send to server
+    message = "shaurya says geeksforgeeks"
+    while True:
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # message sent to server
+        s.send(message.encode('ascii'))
 
-        # connect to server on local computer
-        s.connect((ip, port))
+        # message received from server
+        data = s.recv(1024)
 
-        # message you send to server
-        message = "shaurya says geeksforgeeks"
-        while True:
+        # print the received message
+        # here it would be a reverse of sent message
+        print('Received from the server :', str(data.decode('ascii')))
 
-            # message sent to server
-            s.send(message.encode('ascii'))
+        # ask the client whether he wants to continue
+        ans = input('\nDo you want to continue(y/n) :')
+        if ans == 'y':
+            continue
+        else:
+            break
+    # close the connection
+    s.close()
 
-            # message received from server
-            data = s.recv(1024)
-
-            # print the received message
-            # here it would be a reverse of sent message
-            print('Received from the server :', str(data.decode('ascii')))
-
-            # ask the client whether he wants to continue
-            ans = input('\nDo you want to continue(y/n) :')
-            if ans == 'y':
-                continue
-            else:
-                break
-        # close the connection
-        s.close()
-
-    if __name__ == '__main__':
-        Main()
