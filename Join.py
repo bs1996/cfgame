ef76c6f4f765834a5bcc4ea41ed6e780871e1077
bartek <bartekstefaniak@hotmail.com>
2022-04-30 00:16:22 +0200

    waiting room changes

--- Join.py
+++ Join.py
@@ -5,6 +5,15 @@ from threading import Thread
 def join(ip):
 
     # Define the port on which you want to connect
+    pygame.font.init()
+    pygame.font.get_init()
+    menu = pygame.display.set_mode((600, 600))
+    font1 = pygame.font.SysFont('chalkduster.ttf', 70)
+    text1 = font1.render('waiting for players ', True, (0, 255, 0))
+    textRect1 = text1.get_rect()
+    textRect1.center = (300, 100)
+    menu.blit(text1, textRect1)
+    pygame.display.update()
     port = 12345
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
